5c3c137331271dad942a69d6566fcb41bcd8fa66
Yan Carlos <ycarlospereira@gmail.com>
2022-12-23 19:33:27 -0300

    feat: creation assets, components and screens folders
    creation assets and images folders for adding images, creation  screens folder for screens and creation  components with the addition of colors for the app

---
+++ map/lib/screens/launch_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LaunchScreen extends StatefulWidget {
+  const LaunchScreen({super.key});
+
+  @override
+  State<LaunchScreen> createState() => _LaunchScreenState();
+}
+
+class _LaunchScreenState extends State<LaunchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
